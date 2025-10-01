26d0c647c4d408e0351a24dab5757ca60a240353
dth235633_tranvuduy <duy_dth235633@student.agu.edu.vn>
2025-10-01 21:43:03 +0700

    Nộp bài

--- baitap2.py
+++
@@ -1,8 +0,0 @@
-import math
-
-so = float(input("Nhập vào một số: "))
-if so < 0:
-    print("Không thể tính căn bậc 2 của số âm.")
-else:
-    can_bac_2 = math.sqrt(so)
-    print(f"Căn bậc 2 của {so} là {can_bac_2}")
